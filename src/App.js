dedbf71138187cb7917f3eafac91117b65683535
Arwinder Singh <arwinder7024@gmail.com>
2024-04-07 13:55:16 +0530

    installed unicons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
 function App() {
-  return <h1 className="text-6xl text-color-red">Hello World</h1>;
+  return (
+    <div>
+      <h1 className="text-6xl text-red-500">Hello World</h1>;
+      <UilReact size="140" color="#61DAFB" />
+    </div>
+  );
 }
 
 export default App;
